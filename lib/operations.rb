687c12e5b35e34d084a285410e044c616931d67e
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-15 06:24:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,13 @@
 def unsafe?(speed)
-  speed = 90
   if speed > 60 || speed < 40
     return true
   else
-  end
-  
-  speed = 50
-  if speed > 60 || speed < 40
     return false
-  else
   end
 end
+
+unsafe?(90)
+unsafe?(50)
 
 
 
